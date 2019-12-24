8348816d0f68d96c33260ce15ecc4d85638c803f
Ivan Mwiruki <ivanmwiruki@gmail.com>
2019-12-23 21:13:45 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ ext["kotlintestVersion"] = "3.1.10"
 ext["slf4jVersion"] = "1.7.25"
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.61"
 }
 
 group = "com.30secondsofkotlin"
@@ -16,8 +16,8 @@ dependencies {
     compile(kotlin("stdlib"))
     compile(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC2")
-    testCompile("io.kotlintest:kotlintest-runner-junit5:${project.rootProject.ext["kotlintestVersion"]}")
-    testRuntime("org.slf4j:slf4j-nop:${project.rootProject.ext["slf4jVersion"]}")
+    testCompile("io.kotlintest:kotlintest-runner-junit5:${project.ext["kotlintestVersion"]}")
+    testRuntime("org.slf4j:slf4j-nop:${project.ext["slf4jVersion"]}")
 }
 
 kotlin {
